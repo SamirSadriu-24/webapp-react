9e12fef4d80347a8e26034ebc5e494b38bf072ff
samir <samirsadriuboolean@gmail.com>
2025-11-05 15:59:59 +0100

    creazione form e collegamento con db riuscito

--- src/components/MyMovieDetails.jsx
+++ src/components/MyMovieDetails.jsx
@@ -1,6 +1,7 @@
 import { Link, useParams } from "react-router-dom"
 import { useEffect, useState } from "react";
 import axios from "axios";
+import MyReviewForm from "./MyReviewForm";
 
 const MyMovieDetails = () => {
 
@@ -40,14 +41,18 @@ const MyMovieDetails = () => {
                                 <li className="mt-5">
                                     <h5 className="text-danger">RECENSIONI</h5>
                                     {movie?.reviews?.map((review, i) => (
-                                        <p className="fst-italic text-white" key={i}><span className="fw-bold fst-normal">{review.name}</span>: "{review.text}" <span className="text-white fw-bold pe-2 fst-normal">voto:</span><span className="fw-bold">{review.vote}<span/> <span className="text-danger">/5</span></span></p>
+                                        <p className="fst-italic text-white" key={i}><span className="fw-bold fst-normal">{review.name}</span>: "{review.text}" <span className="text-white fw-bold pe-2 fst-normal">voto:</span><span className="fw-bold">{review.vote}<span /> <span className="text-danger">/5</span></span></p>
                                     ))}
+                                </li>
+                                <li>
+                                    <MyReviewForm movieId={id}/>
                                 </li>
                             </ul>
                         </div>
                     </div>
                     <Link to="/Jumbo" className="back btn btn-danger pulsa">Torna Indietro</Link>
                 </div>
+
             </div>
 
         </div>
---
+++ src/components/MyReviewForm.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import axios from "axios";
+
+const MyReviewForm = ({ movieId }) => {
+
+    const [open, setOpen] = useState(false);
+    const [name, setName] = useState("");
+    const [vote, setVote] = useState("");
+    const [text, setText] = useState("");
+
+    const gestisciInvio = (e) => {
+        e.preventDefault();
+        const review = { name, vote: Number(vote), text };
+
+        axios.post(`http://localhost:3000/movies/${movieId}`, review, {
+            headers: { "Content-Type": "application/json" },
+        })
+            .then((response) => {
+                console.log(response.data)
+                setMessage("Recensione inviata!")
+
+                setName("");
+                setVote("");
+                setText("");
+            })
+
+            .catch((error) => {
+                console.error(error);
+                setMessage("Errore nell'invio della review.");
+            });
+    };
+
+    return (
+        <>
+            <button className="btn btn-danger text-white" onClick={() => setOpen(!open)}> LASCIA LA TUA RECENSIONE</button>
+            <form className={`collapse mt-2 ${open ? "show" : ""}`}
+                    onSubmit={gestisciInvio}>
+                <input
+                    className="form-control mb-2"
+                    type="text"
+                    placeholder="Nome"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                    className="form-control mb-2"
+                    type="number"
+                    placeholder="Voto"
+                    value={vote}
+                    onChange={(e) => setVote(e.target.value)}
+                    min={1}
+                    max={5}
+                />
+                <input
+                    className="form-control mb-2"
+                    type="text"
+                    placeholder="Recensione"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                />
+                <button className="btn btn-danger" type="submit">Invia!</button>
+            </form>
+        </>
+    )
+}
+
+export default MyReviewForm
